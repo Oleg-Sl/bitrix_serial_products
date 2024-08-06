ff0e14dc00db938ce7c093e9ce7c2b8870c6e59c
Oleg <slepcov_oleg@mail.ru>
2024-08-06 19:01:31 +0700

    Added settings file

--- bitrix_serial_products/bitrix_serial_products/settings.py
+++ bitrix_serial_products/bitrix_serial_products/settings.py
@@ -1,16 +1,11 @@
-
+import os
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+SECRET_KEY = ''
 
-SECRET_KEY = 'django-insecure-tg+vib6^=5*a!_dn7!_tkjbrjxan_@omjqyzs%cw-4qi)1(v5j'
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
@@ -25,6 +20,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'api_v1'
 ]
 
 MIDDLEWARE = [
@@ -58,8 +55,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'bitrix_serial_products.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
@@ -100,12 +95,14 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
+STATIC_URL = 'bitrix-crm-card/static/'
+if DEBUG:
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'bitrix-crm-card/static')]
 
-STATIC_URL = 'static/'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOW_CREDENTIALS = True
