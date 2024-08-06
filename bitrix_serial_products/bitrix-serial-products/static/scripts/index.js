312eb7bef1537275745cb3eb4c756f82e96d26fa
Oleg <slepcov_oleg@mail.ru>
2024-08-06 22:08:59 +0700

    Changed gitignore

--- bitrix_serial_products/bitrix-serial-products/static/scripts/index.js
+++ bitrix_serial_products/bitrix-serial-products/static/scripts/index.js
@@ -3,7 +3,9 @@ import { main } from './index/index.js';
 
 
 document.addEventListener("DOMContentLoaded", async function() {
-    const bx24 = new ApiClient();
-    await bx24.init();
-    main(bx24);
+    BX24.init(async function(){
+        const bx24 = new ApiClient();
+        await bx24.init();
+        main(bx24);
+    });
 });
