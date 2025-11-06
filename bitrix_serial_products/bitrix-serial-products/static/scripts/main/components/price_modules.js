94730a25aa65603c00eef39d60a872a06e605658
Sleptsov Oleg <slepcov_oleg@mail.ru>
2025-11-06 23:30:24 +0700

    Add link to economy to card

--- bitrix_serial_products/bitrix-serial-products/static/scripts/main/components/price_modules.js
+++ bitrix_serial_products/bitrix-serial-products/static/scripts/main/components/price_modules.js
@@ -49,7 +49,8 @@ export default class PriceModules {
                 if (!(fabricTitle in data)) {
                     data[fabricTitle] = 0;
                 }
-                data[fabricTitle] += product.productPrices[fabricTitle] * product.productCount;
+                // data[fabricTitle] += product.productPrices[fabricTitle] * product.productCount;
+                data[fabricTitle] += product.productPrices[fabricTitle] * 1;
             }
         }
 
